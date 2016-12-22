0d8387760fc8d1c626ed10f8dedf548c2e75d71e
Brais Gabín <braisgabin@gmail.com>
2016-12-22 11:02:15 +0100

    Add Fabric
    I'm not adding app/fabric.properties

--- app/src/main/java/com/braisgabin/crashlytics_and_rxjava/App.java
+++ app/src/main/java/com/braisgabin/crashlytics_and_rxjava/App.java
@@ -1,11 +1,14 @@
 package com.braisgabin.crashlytics_and_rxjava;
 
 import android.app.Application;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 public class App extends Application {
 
   @Override
   public void onCreate() {
     super.onCreate();
+    Fabric.with(this, new Crashlytics());
   }
 }
