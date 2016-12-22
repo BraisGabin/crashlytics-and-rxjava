26c17d23bd913b2892bd24e60b56dd9fb4b491a5
Brais Gabín <braisgabin@gmail.com>
2016-12-22 10:49:36 +0100

    Add code to reproduce the error

--- app/src/main/java/com/braisgabin/crashlytics_and_rxjava/MainActivity.java
+++ app/src/main/java/com/braisgabin/crashlytics_and_rxjava/MainActivity.java
@@ -1,7 +1,11 @@
 package com.braisgabin.crashlytics_and_rxjava;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import rx.Observable;
+import rx.functions.Func1;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Observable.just("hello")
+            .map(new Func1<String, String>() {
+              @Override
+              public String call(String s) {
+                throw new RuntimeException("Report this to Crashlytics, please!");
+              }
+            })
+            .subscribe();
+      }
+    });
   }
 }
