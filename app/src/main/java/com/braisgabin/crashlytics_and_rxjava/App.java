fec07ed341b83705cb3dd81e6c8a020563122075
Brais Gabín <braisgabin@gmail.com>
2016-12-22 10:52:08 +0100

    Add App

---
+++ app/src/main/java/com/braisgabin/crashlytics_and_rxjava/App.java
@@ -0,0 +1,11 @@
+package com.braisgabin.crashlytics_and_rxjava;
+
+import android.app.Application;
+
+public class App extends Application {
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+  }
+}
